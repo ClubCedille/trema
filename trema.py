acb12b7b9ad4c8d8dcc53a2aec1f292eab595d0d
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-05-28 20:55:09 -0400

    Message when a member joins or leaves

--- trema.py
+++ trema.py
@@ -23,5 +23,40 @@ args = arg_parser.parse_args()
 server_ids = args.serveurs
 bot_token = args.jeton
 
-print(f"Serveurs: {server_ids}")
-print(f"Jeton du robot: {bot_token}")
+intents = discord.Intents.default()
+intents.members = True
+
+trema = discord.Bot(intents=intents)
+
+
+@trema.slash_command(guild_ids=server_ids, name="hello")
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+
+@trema.event
+async def on_member_join(member):
+	guild = member.guild
+	sys_chan = guild.system_channel
+	welcome_msg = "Bonjour"
+#	welcome_msg =\
+#		f"Heille {member.mention}!" +\
+#		f"\nBienvenue au Club CEDILLE. Rends-toi dans {sys_chan.name}" +\
+#		"pour avoir accès au reste du serveur!"
+	await sys_chan.send(welcome_msg)
+
+
+@trema.event
+async def on_member_remove(member):
+	guild = member.guild
+	sys_chan = guild.system_channel
+	message = f"{member.name} décâlisse."
+	await sys_chan.send(message)
+
+
+@trema.event
+async def on_ready():
+    print(f"{trema.user} fonctionne.")
+
+
+trema.run(bot_token)
