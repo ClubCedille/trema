6546930cad0fcbe350186e3926f4a31c7449fe09
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-10-14 15:34:17 -0400

    Module slash_commands updated

--- slash_commands.py
+++ slash_commands.py
@@ -1,4 +1,7 @@
-from discord import Option
+from discord import\
+	Color,\
+	Embed,\
+	Option
 
 
 _ARG_CANAL_ACCUEIL = "canalaccueil"
@@ -20,9 +23,22 @@ def create_slash_cmds(trema_bot, trema_db, server_id):
 		slash_cmd_config(ctx, trema_db, param, value)
 
 
-def slash_cmd_config(ctx, trema_db, param, value):
+async def slash_cmd_config(ctx, trema_db, param, value):
+	embed_title = "Paramètre mis à jour: "
+
 	if param == _ARG_CANAL_ACCUEIL:
+		embed_title += "canal d'accueil"
+		prev_value = trema_db.get_server_welcome_chan_id(ctx.guild_id)
 		trema_db.set_server_welcome_chan_id(ctx.guild_id, int(value))
 
 	elif param == _ARG_MSG_ACCUEIL:
+		embed_title += "message d'accueil"
+		prev_value = trema_db.get_server_welcome_msg(ctx.guild_id)
 		trema_db.set_server_welcome_msg(ctx.guild_id, value)
+
+	confirm_embed = Embed(
+		title=embed_title,
+		description=f"Valeur précédente: {prev_value}\nNouvelle valeur: {value}",
+		color=Color.green())
+
+	await ctx.send(embed=confirm_embed)
