92f7a26288ddca48debeb8ae995f3d916346190a
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-11-08 09:42:41 -0500

    Event member and channel mentioned in a message.

--- events.py
+++ events.py
@@ -4,6 +4,9 @@ from discord_util import\
 	member_roles_are_default,\
 	send_delayed_dm
 
+from msg_format import\
+	format_message
+
 
 _DELAY_SECS_15_MIN = 15 * 60
 
@@ -27,11 +30,13 @@ def create_event_reactions(trema_bot, trema_db):
 		welcome_chan = _get_welcome_chan(guild, trema_db)
 
 		welcome_msg = trema_db.get_server_welcome_msg(guild_id)
+		welcome_msg = format_message(welcome_msg, member)
 		await welcome_chan.send(welcome_msg)
 
 		# A reminder if the new member does not select a role
 		reminder_msg = trema_db.get_server_reminder_msg(guild_id)
 		if not member.bot and reminder_msg is not None:
+			reminder_msg = format_message(reminder_msg, member)
 			msg_condition = lambda: member_roles_are_default(member)
 			reminder_task = asyncio.create_task(send_delayed_dm(
 				member, reminder_msg, _DELAY_SECS_15_MIN, msg_condition))
@@ -41,9 +46,10 @@ def create_event_reactions(trema_bot, trema_db):
 	async def on_member_remove(member):
 		guild = member.guild
 		guild_id = guild.id
-		welcome_chan = _get_welcome_chan(guild, trema_db)
 		leave_msg = trema_db.get_server_leave_msg(guild_id)
 		if leave_msg is not None:
+			welcome_chan = _get_welcome_chan(guild, trema_db)
+			leave_msg = format_message(leave_msg, member)
 			await welcome_chan.send(leave_msg)
 
 	@trema_bot.event
---
+++ msg_format.py
@@ -0,0 +1,50 @@
+import re
+
+from discord_util import\
+	get_channel_by_name,\
+	get_member_by_name
+
+
+_PATTERN_NAME = r"(\w)+"
+
+# #channel
+_PATTERN_CHANNEL = "#" + _PATTERN_NAME
+
+# The member concerned by an event
+_PATTERN_EVENT_MEMBER = "@-"
+
+# @member
+_PATTERN_MEMBER_NAME = "@" + _PATTERN_NAME
+
+_PIPE = "|"
+_FULL_PATTERN = _PATTERN_CHANNEL\
+	+ _PIPE + _PATTERN_EVENT_MEMBER\
+	+ _PIPE + _PATTERN_MEMBER_NAME
+print(_FULL_PATTERN)
+
+
+def format_message(message, event_member):
+	guild = event_member.guild
+
+	def _apply_fomrat(match_obj):
+		match_str = match_obj.group()
+		print(match_str)
+		replacement = None
+
+		if re.fullmatch(_PATTERN_CHANNEL, match_str):
+			channel = get_channel_by_name(guild, match_str[1:])
+			replacement = channel.mention
+
+		elif re.fullmatch(_PATTERN_EVENT_MEMBER, match_str):
+			replacement = event_member.mention
+
+		elif re.fullmatch(_PATTERN_MEMBER_NAME, match_str):
+			guild_member = get_member_by_name(guild, match_str[1:])
+			replacement = guild_member.mention
+
+		print(replacement)
+		return replacement
+
+	formatted_msg = re.sub(_FULL_PATTERN, _apply_fomrat, message)
+	print(formatted_msg)
+	return formatted_msg
